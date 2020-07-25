35c4272304fb844f04af54018268067f8ab1d2c4
Amin Siddiqui <amin.siddiqui.work@gmail.com>
2020-07-25 14:11:45 +0530

    Initial Commit

---
+++ Jet2TT Social/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jet2TT Social
+//
+//  Created by Amin Siddiqui on 25/07/20.
+//  Copyright © 2020 svmLogics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
